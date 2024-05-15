fa7e924bc18aa5ee216c6542271702e1e3f4fa3d
AlexD36 <alexduta36@gmail.com>
2024-05-15 16:13:21 +0300

    springboot db connection

---
+++ HotelReservationSystemSB/src/main/java/com/SphereStay/HotelReservationSystemSB/HotelReservationSystemSbApplication.java
@@ -0,0 +1,13 @@
+package com.SphereStay.HotelReservationSystemSB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelReservationSystemSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelReservationSystemSbApplication.class, args);
+	}
+
+}
---
+++ HotelReservationSystemSB/src/test/java/com/SphereStay/HotelReservationSystemSB/HotelReservationSystemSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SphereStay.HotelReservationSystemSB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelReservationSystemSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
